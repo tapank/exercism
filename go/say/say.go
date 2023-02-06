8c927868cd5861a5c741ac74f9f88883de30dbcd
Tapan Karecha <tapan@karecha.com>
2023-02-06 14:11:39 +0530

    add first cut of solution for say

---
+++ go/say/say.go
@@ -0,0 +1,118 @@
+package say
+
+var units = map[int]string{
+	0:  "",
+	1:  "one",
+	2:  "two",
+	3:  "three",
+	4:  "four",
+	5:  "five",
+	6:  "six",
+	7:  "seven",
+	8:  "eight",
+	9:  "nine",
+	10: "ten",
+	11: "eleven",
+	12: "twelve",
+	13: "thirteen",
+	14: "fourteen",
+	15: "fifteen",
+	16: "sixteen",
+	17: "seventeen",
+	18: "eighteen",
+	19: "nineteen",
+}
+
+var tens = map[int]string{
+	0: "",
+	2: "twenty",
+	3: "thirty",
+	4: "forty",
+	5: "fifty",
+	6: "sixty",
+	7: "seventy",
+	8: "eighty",
+	9: "ninety",
+}
+
+func Say(n int64) (string, bool) {
+	if n < 0 || n > 999_999_999_999 {
+		return "", false
+	}
+	var word string
+	word = SayInt(int(n % 1000))
+	n /= 1000
+	if n == 0 {
+		return word, true
+	}
+	if word == "zero" {
+		if n%1000 > 0 {
+			word = SayInt(int(n%1000)) + " thousand"
+		}
+	} else {
+		if n%1000 > 0 {
+			word = SayInt(int(n%1000)) + " thousand " + word
+		}
+	}
+
+	n /= 1000
+	if n == 0 {
+		return word, true
+	}
+	if word == "zero" {
+		if n%1000 > 0 {
+			word = SayInt(int(n%1000)) + " million"
+		}
+	} else {
+		if n%1000 > 0 {
+			word = SayInt(int(n%1000)) + " million " + word
+		}
+	}
+
+	n /= 1000
+	if n == 0 {
+		return word, true
+	}
+	if word == "zero" {
+		word = SayInt(int(n%1000)) + " billion"
+	} else {
+		word = SayInt(int(n%1000)) + " billion " + word
+	}
+	return word, true
+}
+
+func SayInt(n int) string {
+	if n == 0 {
+		return "zero"
+	}
+	var word string
+	if n%100 > 0 && n%100 < 20 {
+		word = units[n%100]
+		n /= 100
+	} else {
+		word = units[n%10]
+		n /= 10
+		if n == 0 {
+			return word
+		}
+		ten := tens[n%10]
+		if word != "" {
+			word = ten + "-" + word
+		} else {
+			word = ten
+		}
+	}
+	if n /= 10; n == 0 {
+		return word
+	}
+	hundred := units[n%10]
+	if hundred == "" {
+		return word
+	}
+	if word == "" {
+		word = hundred + " hundred"
+	} else {
+		word = hundred + " hundred " + word
+	}
+	return word
+}
