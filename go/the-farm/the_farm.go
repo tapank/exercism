b81c7aa8d73664283e5ce3f59e1e0a48072d87d9
Tapan Karecha <tapan@karecha.com>
2023-09-26 12:38:29 +0530

    update the-farm exercise

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -5,31 +5,51 @@ import (
 	"fmt"
 )
 
+type InvalidCowsError struct {
+	message string
+	//details string
+}
+
+func (e *InvalidCowsError) Error() string {
+	return e.message
+}
+
 // DivideFood computes the fodder amount per cow for the given cows.
-func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	if cows == 0 {
-		return 0.0, errors.New("division by zero")
+func DivideFood(fCalc FodderCalculator, cows int) (float64, error) {
+	if err := ValidateNumberOfCows(cows); err != nil {
+		return 0, err
 	}
-	fAmt, fErr := weightFodder.FodderAmount()
+	fAmt, fErr := fCalc.FodderAmount(cows)
 	if fErr != nil {
-		if fErr == ErrScaleMalfunction {
-			if fAmt < 0 {
-				return 0, errors.New("negative fodder")
-			} else if cows < 0 {
-				return 0, fmt.Errorf("silly nephew, there cannot be %d cows", cows)
-			}
-			return fAmt * 2 / float64(cows), nil
-		} else {
-			if fAmt < 0 {
-				return 0.0, errors.New("non-scale error")
-			}
-			return 0, errors.New("non-scale error")
-		}
+		return 0, fErr
 	}
+
+	fFactor, faErr := fCalc.FatteningFactor()
+	if faErr != nil {
+		return 0, faErr
+	}
+
 	if fAmt < 0 {
 		return 0, errors.New("negative fodder")
-	} else if cows < 0 {
-		return 0, fmt.Errorf("silly nephew, there cannot be %d cows", cows)
+	} else if fAmt == 0 {
+		return 0, errors.New("amount could not be determined")
 	}
-	return fAmt / float64(cows), nil
+
+	return fAmt * fFactor / float64(cows), nil
+}
+
+func ValidateInputAndDivideFood(fc FodderCalculator, cows int) (float64, error) {
+	if err := ValidateNumberOfCows(cows); err != nil {
+		return 0, errors.New("invalid number of cows")
+	}
+	return DivideFood(fc, cows)
+}
+
+func ValidateNumberOfCows(cows int) error {
+	if cows > 0 {
+		return nil
+	} else if cows == 0 {
+		return fmt.Errorf("%d cows are invalid: no cows don't need food", cows)
+	}
+	return fmt.Errorf("%d cows are invalid: there are no negative cows", cows)
 }
